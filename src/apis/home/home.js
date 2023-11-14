09371397f696af46d2a0b28782cd99ffcc4d0956
weiyouming <weiyouming@homedo.com>
2023-11-14 13:04:57 +0800

    调整配置

--- src/apis/home/home.js
+++ src/apis/home/home.js
@@ -1,13 +1,13 @@
 import request from '../../utils/request';
 
 export function getOpenId() {
-	return request.get('/getOpenId');
+	return request.get('/api/getOpenId');
 }
 
 export function requestIndex(param, option) {
-	return request.get('/');
+	return request.get('/api/');
   }
 
 export function change(param, option) {
-  return request.post('/user/modifyPwd', param, option);
+  return request.post('/api/modifyPwd', param, option);
 }
